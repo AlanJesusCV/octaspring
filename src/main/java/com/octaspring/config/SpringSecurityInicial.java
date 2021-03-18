27d1b34d249b9fc1c82b1c5998d4924e4644f58e
AlanJesusCV <74394276+AlanJesusCV@users.noreply.github.com>
2021-03-18 11:11:55 -0600

    Delete src/main/java/com/octaspring directory

--- src/main/java/com/octaspring/config/SpringSecurityInicial.java
+++
@@ -1,7 +0,0 @@
-package com.octaspring.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInicial extends AbstractSecurityWebApplicationInitializer{
-
-}
